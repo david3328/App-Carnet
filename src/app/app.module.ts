f324833852fb3f24cf1f4111dd79055e6428d718
David <davidjsupoo@gmail.com>
2019-04-30 13:00:23 -0500

    Se importan los componentes generados y los módulogs HttpCliente(para realizar peticiones a la API) y Forms(para el data binding con el input)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AcademiaComponent } from './components/carnet/academia/academia.component';
+import { ColegioComponent } from './components/carnet/colegio/colegio.component';
+import { EvolisComponent } from './components/carnet/evolis/evolis.component';
+import { EvolisrfidComponent } from './components/carnet/evolisrfid/evolisrfid.component';
+import { CodebarPipe } from './pipes/codebar.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AcademiaComponent,
+    ColegioComponent,
+    EvolisComponent,
+    EvolisrfidComponent,
+    CodebarPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
